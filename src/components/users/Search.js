937bdb2ac0be58909e03461b78ff50f786715b2e
Arun Locham <arlm907204@aol.com>
2021-08-22 01:01:29 +0530

    refactor using hooks, contexts and finish the project

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,8 +1,12 @@
-import React, { useState } from 'react';
-import PropTypes from 'prop-types';
+import React, { useState, useContext } from 'react';
+import GithubContext from '../../context/github/githubContext';
+import AlertContext from '../../context/alert/alertContext';
 
 
-const Search = ({ searchUsers, showClear, clearUsers, setAlert }) => {
+const Search = () => {
+
+    const githubContext = useContext(GithubContext);
+    const alertContext = useContext(AlertContext);
 
     const [text, setText] = useState('');
 
@@ -10,9 +14,9 @@ const Search = ({ searchUsers, showClear, clearUsers, setAlert }) => {
     const onSubmit = e => {
         e.preventDefault();
         if (text === '') {
-            setAlert('Please enter something', 'light');
+            alertContext.setAlert('Please enter something', 'light');
         } else {
-            searchUsers(text);
+            githubContext.searchUsers(text);
             setText('');
         }
 
@@ -26,17 +30,10 @@ const Search = ({ searchUsers, showClear, clearUsers, setAlert }) => {
                 <input type="text" name="text" placeholder="Search Users..." onChange={onChange} value={text} />
                 <input type="submit" className="btn btn-dark btn-block" value="Search" />
             </form>
-            {showClear && <button className="btn btn-light btn-block" onClick={clearUsers}>Clear</button>}
+            {githubContext.users.length > 0 && <button className="btn btn-light btn-block" onClick={githubContext.clearUsers}>Clear</button>}
 
         </div>
     );
-};
-
-Search.propTypes = {
-    searchUsers: PropTypes.func.isRequired,
-    clearUsers: PropTypes.func.isRequired,
-    showClear: PropTypes.bool.isRequired,
-    setAlert: PropTypes.func.isRequired
 };
 
 export default Search;
